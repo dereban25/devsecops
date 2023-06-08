44cd33c560c17722fb48d1188d465e9fbfb56cf6
Maxim Derbenev <dereban25@gmail.com>
2023-06-08 17:04:10 -0400

    Update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 command -v gitleaks >/dev/null 2>&1 || { 
-    echo >&2 "Gitleaks не знайдено. Встановлюю gitleaks...";
+    echo >&2 "Gitleaks not found. I will install gitleaks...";
     
-    # Встановлення gitleaks залежно від операційної системи
+    # Installed gitleaks in the operating system
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         curl -sSfL https://github.com/zricethezav/gitleaks/releases/latest/download/gitleaks-linux-amd64.tar.gz | tar -xz
-        export PATH=$(pwd):$PATH  # Додавання поточного каталогу до шляху пошуку
+        export PATH=$(pwd):$PATH
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         curl -sSfL https://github.com/gitleaks/gitleaks/releases/download/v8.16.4/gitleaks_8.16.4_darwin_arm64.tar.gz | tar -xz
-        export PATH=$(pwd):$PATH  # Додавання поточного каталогу до шляху пошуку
+        export PATH=$(pwd):$PATH
     else
-        echo >&2 "Не вдалося встановити gitleaks. Операційна система не підтримується."; 
+        echo >&2 "Couldn't install gitleaks. The operating system is not supported."; 
         exit 1;
     fi
 }
 
-# Перевірка значення опції enable у git config
+
 ENABLE=$(git config --bool hooks.gitleaks-enable)
 
-# Перевірка, чи ввімкнена опція enable
+# Check if the enable option is enabled
 if [ "$ENABLE" != "true" ]; then
-    echo "Попередження: Перевірка наявності секретів у коді вимкнена. Для включення виконайте команду: git config hooks.gitleaks-enable true"
+    echo "Advance: Re-verification of the visibility of secrets in the code of the code. To enable, type the command: git config hooks.gitleaks-enable true"
     exit 0
 fi
 
-# Запуск gitleaks для перевірки наявності секретів
+
 gitleaks detect
 
-# Перевірка коду вихідного повідомлення gitleaks
+
 if [ $? -eq 0 ]; then
-    echo "Помилка: Знайдено секрети у коді. Відхиляю коміт."
+    echo "Pardon: Found the secret of the code. I resigned the committee."
     exit 1
 fi
 
-# Коміт проходить перевірку, якщо немає знайдених секретів
 exit 0
 
